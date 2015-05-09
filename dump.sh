92e57964c4d7c617ec85394d191592f25ab030aa
root <root@homiq.promienko.pl>
2015-05-09 22:21:36 +0200

    dump

--- dump.sh
+++ dump.sh
@@ -8,7 +8,9 @@ pg_dump homiq-promienko > homiq-promienko.sql
 rm -f homiq-promienko.sql.gz
 gzip homiq-promienko.sql
 vacuumdb -f -d homiq-promienko >/dev/null
-svn -q -m "baza danych" ci homiq-promienko.sql.gz
+#svn -q -m "baza danych" ci homiq-promienko.sql.gz
+git commit -m "baza danych" homiq-promienko.sql.gz
+git push
 
 if [ "`id -u`" = "0" ]
 then
