a82fe70ec616a3018f98951eb7eeefeb874be45e
Piotr Homiq <piotr@webkameleon.com>
2015-12-13 14:06:56 +0100

    better

--- start-homiq.sh
+++ start-homiq.sh
@@ -1,2 +1,16 @@
-sleep 5
+
+while [ "1" = "1" ]
+do
+	temp=`echo "select g_t from global;" |psql -t -q homiq-promienko 2>/dev/null|head -n 1 `
+
+
+	if [ "$temp" ]
+	then
+		break
+	fi
+	sleep 1
+	echo dupa
+done
+
+
 /home/homiq/start.sh >/dev/null 2>/dev/null
