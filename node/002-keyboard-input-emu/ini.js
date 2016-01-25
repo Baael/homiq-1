20ffcf3cf879c917303637e88441958621e96982
Piotr Homiq <piotr@webkameleon.com>
2016-01-25 11:04:47 +0100

    ssh

--- node/002-keyboard-input-emu/ini.js
+++ node/002-keyboard-input-emu/ini.js
@@ -2,7 +2,6 @@ module.exports = {
     port: 4001,
     listen: '127.0.0.1',
     http_port: 8002,
-    tunnel_to: 'homiq2@gold.webkameleon.com',
-    tunnel_to2: 'homiqw23@localhost',
+    tunnel_to: 'homiq@homiq.webkameleon.com',
     tunnel_port: 8001
 }
